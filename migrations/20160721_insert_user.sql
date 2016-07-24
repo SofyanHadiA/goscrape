c7a4d7cf8722821190a5c75109b6ea22df744081
Sofyan Hadi A <sofyan.h.ahmad@gmail.com>
2016-07-25 01:14:29 +0700

    Update 20160721_insert_user.sql

--- migrations/20160721_insert_user.sql
+++ migrations/20160721_insert_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `passsword`) VALUES ('1', '1', 'User', 'user');
-INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `passsword`) VALUES ('2', '2', 'Admin', 'admin');
-INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `passsword`) VALUES ('3', '3', 'Super Admin', 'sa');
+INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `password`) VALUES ('1', '1', 'User', 'user');
+INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `password`) VALUES ('2', '2', 'Admin', 'admin');
+INSERT INTO `scraper`.`users` (`id`, `role_id`, `name`, `password`) VALUES ('3', '3', 'Super Admin', 'sa');
